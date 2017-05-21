a6c7ec64d9a95010bb2e138ffabf2b058219e8be
SuddenFX <support@suddenfx.com>
2017-05-21 17:03:46 -0500

    Added Debian and Wine Installers

---
+++ install-deb.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+set -e
+
+if ! [[ -x $(which git) ]]; then
+  apt-get install git
+fi
+
+cd /tmp
+git clone git://github.com/caarlos0/msfonts.git
+
+echo "copying fonts to /usr/share/fonts/TTF/..."
+mkdir -p /usr/share/fonts/TTF/
+cp /tmp/msfonts/fonts/* /usr/share/fonts/TTF/ -rf
+
+echo "updating all caches..."
+fc-cache -vf
+mkfontscale
+mkfontdir
+
+echo "done."
+
